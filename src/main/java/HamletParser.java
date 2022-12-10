58603300d7fe950f378213ad1b940b0f974c50dd
zprongua <115650795+zprongua@users.noreply.github.com>
2022-12-10 13:11:20 -0500

    Found and Replaced all required text using Pattern/Matcher using TDD.  100%/100%/97%

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,6 +10,10 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    Pattern p1 = Pattern.compile("Hamlet");
+    Pattern p2 = Pattern.compile("HAMLET");
+    Pattern p3 = Pattern.compile("Horatio");
+    Pattern p4 = Pattern.compile("HORATIO");
 
     public HamletParser(){
         this.hamletData = loadFile();
@@ -16,15 +22,15 @@ public class HamletParser {
     private String loadFile(){
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
-        StringBuilder result = new StringBuilder("");
+        StringBuilder result = new StringBuilder();
 
         try(Scanner scanner = new Scanner(file)){
             while(scanner.hasNextLine()){
                 String line = scanner.nextLine();
+                line = changeHamletToLeon(line);
+                line = changeHoratioToTariq(line);
                 result.append(line).append("\n");
             }
-
-            scanner.close();
         }catch(IOException e){
             e.printStackTrace();
         }
@@ -36,4 +42,43 @@ public class HamletParser {
         return hamletData;
     }
 
+    public int findHoratio(String horatio) {
+        Matcher m1 = p3.matcher(horatio);
+        Matcher m2 = p4.matcher(horatio);
+
+        if (m1.find()) return 1;
+        else if (m2.find()) return 2;
+        return -1;
+    }
+
+    public int findHamlet(String hamlet) {
+        Matcher m1 = p1.matcher(hamlet);
+        Matcher m2 = p2.matcher(hamlet);
+
+        if (m1.find()) return 1;
+        else if (m2.find()) return 2;
+        return -1;
+    }
+
+    public String changeHamletToLeon(String input) {
+        Matcher m1 = p1.matcher(input);
+        Matcher m2 = p2.matcher(input);
+        String r1 = "Leon";
+        String r2 = "LEON";
+        int index = findHamlet(input);
+        if (index == 1) return m1.replaceAll(r1);
+        else if (index == 2) return m2.replaceAll(r2);
+        return input;
+    }
+
+    public String changeHoratioToTariq(String input) {
+        Matcher m1 = p3.matcher(input);
+        Matcher m2 = p4.matcher(input);
+        String r1 = "Tariq";
+        String r2 = "TARIQ";
+        int index = findHoratio(input);
+        if (index == 1) return m1.replaceAll(r1);
+        else if (index == 2) return m2.replaceAll(r2);
+        return input;
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -14,18 +14,93 @@ public class HamletParserTest {
     }
 
     @Test
-    public void testChangeHamletToLeon() {
+    public void testResult() {
+//        System.out.println(hamletText);
+        assertEquals(-1, hamletParser.findHamlet(hamletText));
+        assertEquals(-1, hamletParser.findHoratio(hamletText));
     }
 
     @Test
-    public void testChangeHoratioToTariq() {
+    public void testChangeHamletToLeon1() {
+        HamletParser hp = new HamletParser();
+
+        String input = "Hello, my name is Hamlet, you killed my father, prepare to die";
+        String expected = "Hello, my name is Leon, you killed my father, prepare to die";
+        String actual = hp.changeHamletToLeon(input);
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void testFindHoratio() {
+    public void testChangeHamletToLeon2() {
+        HamletParser hp = new HamletParser();
+
+        String input = "Hello, my name is HAMLET, you killed my father, prepare to die";
+        String expected = "Hello, my name is LEON, you killed my father, prepare to die";
+        String actual = hp.changeHamletToLeon(input);
+
+        assertEquals(expected, actual);
     }
 
     @Test
-    public void testFindHamlet() {
+    public void testChangeHoratioToTariq1() {
+        HamletParser hp = new HamletParser();
+
+        String input = "Hello, my name is Horatio, you killed my father, prepare to die";
+        String expected = "Hello, my name is Tariq, you killed my father, prepare to die";
+        String actual = hp.changeHoratioToTariq(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testChangeHoratioToTariq2() {
+        HamletParser hp = new HamletParser();
+
+        String input = "Hello, my name is HORATIO, you killed my father, prepare to die";
+        String expected = "Hello, my name is TARIQ, you killed my father, prepare to die";
+        String actual = hp.changeHoratioToTariq(input);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test // proper noun case
+    public void testFindHoratio1() {
+        HamletParser hp = new HamletParser();
+        String horatio = "Hello, my name is Horatio, you killed my father, prepare to die";
+        String inigo = "Hello, my name is Inigo Montoya, you killed my father, prepare to die";
+
+        assertEquals(1, hp.findHoratio(horatio));
+        assertEquals(-1, hp.findHoratio(inigo));
+    }
+
+    @Test // all upper case
+    public void testFindHoratio2() {
+        HamletParser hp = new HamletParser();
+        String horatio = "Hello, my name is HORATIO, you killed my father, prepare to die";
+        String inigo = "Hello, my name is INIGO Montoya, you killed my father, prepare to die";
+
+        assertEquals(2, hp.findHoratio(horatio));
+        assertEquals(-1, hp.findHoratio(inigo));
+    }
+
+    @Test // proper noun case
+    public void testFindHamlet1() {
+        HamletParser hp = new HamletParser();
+        String hamlet = "Hello, my name is Hamlet, you killed my father, prepare to die";
+        String inigo = "Hello, my name is Inigo Montoya, you killed my father, prepare to die";
+
+        assertEquals(1, hp.findHamlet(hamlet));
+        assertEquals(-1, hp.findHamlet(inigo));
+    }
+
+    @Test // all upper case
+    public void testFindHamlet2() {
+        HamletParser hp = new HamletParser();
+        String hamlet = "Hello, my name is HAMLET, you killed my father, prepare to die";
+        String inigo = "Hello, my name is INIGO Montoya, you killed my father, prepare to die";
+
+        assertEquals(2, hp.findHamlet(hamlet));
+        assertEquals(-1, hp.findHamlet(inigo));
     }
 }
